e803d023e0c3df20d83c9a0799c38e764eeb476f
SiwarZaghmi <96624768+SiwarZaghmi@users.noreply.github.com>
2022-03-10 09:34:20 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,66 +1,46 @@
 import requests
 import pymongo
-#from pymongo import mongo_client
-
-url = 'https://api.github.com/repos/SiwarZaghmi/GitHub_Analytics/issues'
-response = requests.get(url)
-data = response.json()
-#print (data)
-
-def delete_issue_information(data):
- for issue in data :
-    del issue['repository_url']
-    del issue['labels_url']
-    del issue['comments_url']
-    del issue['events_url']
-    del issue['html_url']
-    del issue['timeline_url']
-    del issue['active_lock_reason']
-    del issue['assignee']
-    del issue['user']
-    del issue['author_association']
-    del issue['reactions']
-    del issue['performed_via_github_app']
-
-# Nettyotage des donneés du labels
-
-def clean_labels(j):
-    for issue in j:
-        labels = issue['labels']
-        for label in labels:
-             del label['default']
-
-def clean_assignees(j):
-    for issue in j:
-        assignees = issue['assignees']
-        for assignee in assignees:
-            del assignee['html_url']
-            del assignee['followers_url']
-            del assignee['gists_url']
-            del assignee['starred_url']
-            del assignee['subscriptions_url']
-            del assignee['organizations_url']
-            del assignee['received_events_url']
-            del assignee['gravatar_id']
-            del assignee['url']
-
-
-#print(data)
-#appel des fonctions
-clean_labels(data)
-clean_assignees(data)
-delete_issue_information(data)
-print(data[6])
-
-
+token = "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"
+headers = { 'Authorization' : "token {}". format(token)}
 client = pymongo.MongoClient('mongodb://127.0.0.1:27017/')
 mydb = client['GitHubProject']
+# les collections
+issues_collection = mydb.issues
+labels_collection = mydb.labels
+project_card_collection = mydb.project_cards
+assigness_collection = mydb.assignees
+milestone_collection = mydb.milestones
+#get project
+project = requests.get('https://api.github.com/projects/14134007/columns', headers = headers).json()
+#parcourir les colonnes
 
-info = mydb.issues
-info.insert_many(data)
 
-"""
+for column in project:
 
-info1 = mydb.events
-info1.insert_many(ev)
-"""
+ column_name = column['name']
+ print( column['name'])
+ cards = requests.get(column['cards_url'], headers=headers).json()
+ try:
+   for card in cards:
+       content = card['content_url']
+       issue = requests.get(content, headers=headers).json()
+
+       issues_collection.insert_one({
+               'id': issue['id'],
+               'node_id': issue['node_id'],
+               'number': issue['number'],
+               'title': issue['title'],
+               'state': issue['state'],
+               'locked': issue['locked'],
+               'created_at': issue['created_at'],
+               'updated_at': issue['updated_at'],
+               'closed_at': issue['closed_at'],
+               'body': issue['body']
+
+           })
+
+       project_card_collection.insert_one({
+           'issue_id': issue['node_id'],
+           'card_name': column_name,
+           'id': card['id'],
+       })
